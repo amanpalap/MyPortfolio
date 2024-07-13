8a729d3c4c34fc6565fe7e3b522a259c1e7e9b97
amanpalap <pal90589500@gmail.com>
2024-07-13 22:55:30 +0530

    update 2 files, create 5 files and delete 1 file

---
+++ src/app/projects/page.tsx
@@ -0,0 +1,88 @@
+import React from "react";
+import { HeroParallax } from "@/components/ui/hero-parallax";
+import a from "@/images/1.png"
+import b from "@/images/2.png"
+import c from "@/images/3.png"
+
+export default function HeroParallaxDemo() {
+    return <HeroParallax products={products} />;
+}
+
+export const products = [
+    {
+        title: "Moonbeam",
+        link: "https://nerozen.netlify.app/",
+        thumbnail: a,
+    },
+    {
+        title: "Cursor",
+        link: "https://nerozen.netlify.app/",
+        thumbnail: b,
+    },
+    {
+        title: "Rogue",
+        link: "https://mockup1-auth.netlify.app/",
+        thumbnail: c,
+    },
+    {
+        title: "Moonbeam",
+        link: "https://nerozen.netlify.app/",
+        thumbnail: a,
+    },
+    {
+        title: "Cursor",
+        link: "https://nerozen.netlify.app/",
+        thumbnail: b,
+    },
+    {
+        title: "Rogue",
+        link: "https://mockup1-auth.netlify.app/",
+        thumbnail: c,
+    },
+    {
+        title: "Moonbeam",
+        link: "https://nerozen.netlify.app/",
+        thumbnail: a,
+    },
+    {
+        title: "Cursor",
+        link: "https://nerozen.netlify.app/",
+        thumbnail: b,
+    },
+    {
+        title: "Rogue",
+        link: "https://mockup1-auth.netlify.app/",
+        thumbnail: c,
+    },
+    {
+        title: "Moonbeam",
+        link: "https://nerozen.netlify.app/",
+        thumbnail: a,
+    },
+    {
+        title: "Cursor",
+        link: "https://nerozen.netlify.app/",
+        thumbnail: b,
+    },
+    {
+        title: "Rogue",
+        link: "https://mockup1-auth.netlify.app/",
+        thumbnail: c,
+    },
+    {
+        title: "Moonbeam",
+        link: "https://nerozen.netlify.app/",
+        thumbnail: a,
+    },
+    {
+        title: "Cursor",
+        link: "https://nerozen.netlify.app/",
+        thumbnail: b,
+    },
+    {
+        title: "Rogue",
+        link: "https://mockup1-auth.netlify.app/",
+        thumbnail: c,
+    },
+];
+
--- src/components/ui/hero-parallax.tsx
+++ src/components/ui/hero-parallax.tsx
@@ -16,7 +16,7 @@ export const HeroParallax = ({
     products: {
         title: string;
         link: string;
-        thumbnail: string;
+        thumbnail: any;
     }[];
 }) => {
     const firstRow = products.slice(0, 5);
@@ -57,7 +57,7 @@ export const HeroParallax = ({
     return (
         <div
             ref={ref}
-            className="h-[300vh] py-40 overflow-hidden  antialiased relative flex flex-col self-auto [perspective:1000px] [transform-style:preserve-3d]"
+            className="h-[300vh] bg-black py-40 overflow-hidden text-white antialiased relative flex flex-col self-auto [perspective:1000px] [transform-style:preserve-3d]"
         >
             <Header />
             <motion.div
@@ -105,13 +105,8 @@ export const Header = () => {
     return (
         <div className="max-w-7xl relative mx-auto py-20 md:py-40 px-4 w-full  left-0 top-0">
             <h1 className="text-2xl md:text-7xl font-bold dark:text-white">
-                The Ultimate <br /> development studio
+                My Projects
             </h1>
-            <p className="max-w-2xl text-base md:text-xl mt-8 dark:text-neutral-200">
-                We build beautiful products with the latest technologies and frameworks.
-                We are a team of passionate developers and designers that love to build
-                amazing products.
-            </p>
         </div>
     );
 };
