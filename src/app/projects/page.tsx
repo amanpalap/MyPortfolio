294b72bd2ccd9556f903d3dd2777bdb9eb24aa16
amanpalap <pal90589500@gmail.com>
2024-08-11 12:59:43 +0530

    update 1 file and delete 3 files

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,74 +1,76 @@
 import React from "react";
 import { HeroParallax } from "@/components/ui/hero-parallax";
-import a from "@/images/1.png"
-import b from "@/images/2.png"
-import c from "@/images/3.png"
-
 
 
 export default function HeroParallaxDemo() {
 
     const products = [
         {
-            title: "Nerozen",
-            link: "https://nerozen.netlify.app/",
-            thumbnail: a,
-        },
-        {
-            title: "Weather-App",
-            link: "https://customweatherappresponsive.netlify.app/",
-            thumbnail: b,
-        },
-        {
-            title: "Fyle",
-            link: "https://fyle-jade.vercel.app/",
-            thumbnail: c,
+            title: "Green Grocers",
+            link: "https://greengrocerss.netlify.app/",
+            thumbnail: "/grocers.png",
         },
         {
             title: "Nerozen",
             link: "https://nerozen.netlify.app/",
-            thumbnail: a,
+            thumbnail: "/1.png",
         },
         {
             title: "Weather-App",
             link: "https://customweatherappresponsive.netlify.app/",
-            thumbnail: b,
+            thumbnail: "/2.png",
         },
         {
             title: "Fyle",
             link: "https://fyle-jade.vercel.app/",
-            thumbnail: c,
+            thumbnail: "/3.png",
+        },
+        {
+            title: "Todos",
+            link: "https://todosapicomponent.netlify.app/",
+            thumbnail: "/todo.png",
+        },
+        {
+            title: "Fyle",
+            link: "https://fyle-jade.vercel.app/",
+            thumbnail: "/3.png",
+        },
+        {
+            title: "Todos",
+            link: "https://todosapicomponent.netlify.app/",
+            thumbnail: "/todo.png",
+        },
+        {
+            title: "College Insight",
+            link: "https://collegeinsights.netlify.app/",
+            thumbnail: "/collegeInsight.png",
+        },
+        {
+            title: "Green Grocers",
+            link: "https://greengrocerss.netlify.app/",
+            thumbnail: "/grocers.png",
         },
         {
             title: "Nerozen",
             link: "https://nerozen.netlify.app/",
-            thumbnail: a,
+            thumbnail: "/1.png",
         },
         {
-            title: "Weather-App",
-            link: "https://customweatherappresponsive.netlify.app/",
-            thumbnail: b,
+            title: "Green Grocers",
+            link: "https://greengrocerss.netlify.app/",
+            thumbnail: "/grocers.png",
         },
         {
             title: "Fyle",
             link: "https://fyle-jade.vercel.app/",
-            thumbnail: c,
+            thumbnail: "/3.png",
         },
         {
             title: "Nerozen",
             link: "https://nerozen.netlify.app/",
-            thumbnail: a,
+            thumbnail: "/1.png",
         },
-        {
-            title: "Weather-App",
-            link: "https://customweatherappresponsive.netlify.app/",
-            thumbnail: b,
-        },
-        {
-            title: "Fyle",
-            link: "https://fyle-jade.vercel.app/",
-            thumbnail: c,
-        },
+
     ];
 
 
